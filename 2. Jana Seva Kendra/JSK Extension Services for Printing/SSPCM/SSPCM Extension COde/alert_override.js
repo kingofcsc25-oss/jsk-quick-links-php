293fba64357c957202c16931eb30db073f8a42f5
kingofcsc25-oss <kingofcsc25@gmail.com>
2026-07-26 04:36:04 +0530

    Revert "Website  fix index.htmls"
    This reverts commit ba1568458f4cd440c2a24482c1ccbb12885fdc33.

--- 2. Jana Seva Kendra/JSK Extension Services for Printing/SSPCM/SSPCM Extension COde/alert_override.js
+++
@@ -1,8 +0,0 @@
-const originalAlert = window.alert;
-window.alert = function(msg) {
-  if (msg && typeof msg === 'string' && msg.toLowerCase().includes('successfully')) {
-    console.log('SSP Auto-dismissed native alert:', msg);
-    return;
-  }
-  return originalAlert.call(window, msg);
-};
